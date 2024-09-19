6a39d47b948c65c2d23be13e28518c2aeb4af8f4
Anderson <andersonpayao018@gmail.com>
2024-09-19 13:40:57 -0300

    Fundamentos Dart

--- dart/lib/media_de_nota.dart
+++ dart/lib/media_de_nota.dart
@@ -1,5 +1,35 @@
 void main() {
 
-  
+  import 'dart:io';
+
+void main() {
+  double somaNotas = 0;
+
+  for (int i = 1; i <= 4; i++) {
+    print("Digite a nota $i (de 0 a 10): ");
+    double nota = double.parse(stdin.readLineSync()!);
+
+    // Verifica se a nota está no intervalo permitido
+    while (nota < 0 || nota > 10) {
+      print("Nota inválida! Digite uma nota entre 0 e 10: ");
+      nota = double.parse(stdin.readLineSync()!);
+    }
+
+    somaNotas += nota;
+  }
+
+  double media = somaNotas / 4;
+
+  print("Média: $media");
+
+  if (media < 5.0) {
+    print("Classificação: Reprovado");
+  } else if (media >= 7.0) {
+    print("Classificação: Aprovado");
+  } else {
+    print("Classificação: Recuperação");
+  }
+}
+
   
 }
